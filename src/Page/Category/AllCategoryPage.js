9a13e8616c2a05d7ba1afbf99f5a0c2962539202
Karem Shabana <km425360@gmail.com>
2022-07-07 13:45:02 +0200

    structuring redux

--- src/Page/Category/AllCategoryPage.js
+++ src/Page/Category/AllCategoryPage.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CategoryContainer from '../../Components/Category/CategoryContainer';
 import Pagination from '../../Components/Uitily/Pagination';
+import { useSelector, useDispatch } from 'react-redux/es/exports';
+import { getAllCategory } from './../../redux/actions/categoryAction';
 
 const AllCategoryPage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllCategory());
+  }, []);
+
+  const data = useSelector((state) => state.allCategory.category);
+  const loading = useSelector((state) => state.allCategory.loading);
+  console.log(data);
+  console.log(loading);
+
   return (
     <div style={{ minHeight: '670px' }}>
       <CategoryContainer />
